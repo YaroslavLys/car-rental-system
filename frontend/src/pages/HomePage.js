ca6687415c7d8629a993afcd56f71fa341f6f3d9
YaroslavLys <74725159+YaroslavLys@users.noreply.github.com>
2023-05-28 18:37:35 +0300

    Created home page

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import {useContext} from "react";
+import UserWelcome from "../components/UserWelcome";
+import AuthContext from "../context/AuthContext";
+
+const Home = () => {
+    const {user} = useContext(AuthContext);
+    return (
+        <section>
+            {user && <UserWelcome user={user}/>}
+            <h1>You are on home page!</h1>
+        </section>
+    );
+};
+
+export default Home;
